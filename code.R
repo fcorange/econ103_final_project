5acd7521a575d0491d9baaf63d908a801e66f7da
Edward Cai <edwsurewin@Edwards-MacBook-Air.local>
2013-12-08 03:30:49 -0800

    Edward Cai first draft initialization added to Coded.R

--- code.R
+++ code.R
@@ -37,24 +37,85 @@ h <- function(g, x) { #Zixiao
   return(log(g(x)))
 }
 
-T_k <- vector()
-
 # ---- Calculate Derivative of a function ----
 library("numDeriv")
 # ----
 
+### Edward's DRAFT functions START ###
 
-u_k <- function(x) { #Edward
-  
+k<-20
+vec <- c(-2,2) # global Domain
+#vec <- c(-1,1) # global D
+T_k <- rep(0,k) # global T_k
+
+# func and deriv #
+#func <- function(x)  ((2*pi)^-0.5)*exp(-(x)^2/2) # given function as standard normal
+func <- function(x)  exp(x)/((1+exp(x))^2) # given function as logistic
+body(func) <- deriv(body(func), "x")
+
+# evenly find T_k in D #
+compute_T_k <- function(k){ # draw k integers from domain D
+  T_k[1] <- vec[1]
+  T_k[k] <- vec[2]
+  for (i in 2:(k-1)){
+    T_k[i] <- T_k[i-1] + (vec[2]-vec[1])/(k-1)
+  }
+  return(T_k)
 }
 
-s_k <- function(x) { #Edward
-  
+T_k <- compute_T_k(k) # update global variable T_k
+h_x <- sapply(T_k, func) # function evaluated at T_k
+h_prime_x <- 0
+for (i in 1:length(h_x)){  # derivatives evaluated at T_k
+  h_prime_x[i] <- unlist(attributes(func(T_k[i])))
 }
 
-l_k <- function() { #Edward
-  
+# z coordinates #
+z <- rep (0, k-1) # tangent line intersections
+for (i in 1:k-1){
+  z[i] <- (h_x[i+1]-h_x[i]-T_k[i+1]*h_prime_x[i+1]+T_k[i]*h_prime_x[i])/(h_prime_x[i]-h_prime_x[i+1])
 }
+
+# upper bound #
+u_k <- function(x) {
+  if ((T_k[1] <= x) && (x < z[1])){
+    return(h_x[1] + (x-T_k[1])*h_prime_x[1])
+  }
+  for (i in 1:(length(T_k)-2)){
+    if(z[i] <= x && x < z[i+1]){
+      return(h_x[i+1] + (x-T_k[i+1])*h_prime_x[i+1])
+    }
+  }
+  if ((z[k-1] <= x) && (x <= T_k[k])){
+    return(h_x[k] + (x-T_k[k])*h_prime_x[k])
+  }
+}
+
+# S_k transformation where x sampled from u_k #
+S_k <- function(x) {
+  area <- 0 
+  area <- area + (z[1]-T_k[1])*(u_k(z[1])+func(T_k[1])[1])/2
+  area <- area + (T_k[k]-z[k-1])*(u_k(z[k-1])+func(T_k[k])[1])/2
+  for (i in 1:(length(T_k)-2)){
+    area <- area + (z[i+1]-z[i])*(u_k(z[i])+u_k(z[i]))/2
+  }
+  return(exp(u_k(x))/area)
+}
+
+# lower bound #
+l_k <- function(x) {
+  for (i in 1:(length(T_k)-1)){
+    if(T_k[i] <= x && x < T_k[i+1]){
+      return(((T_k[i+1]-x)*h_x[i]+(x-T_k[i])*h_x[i+1])/(T_k[i+1]-T_k[i]))
+    }
+  }
+  if (x == T_k[i+1]){
+    return(((T_k[k]-x)*h_x[k-1]+(x-T_k[k-1])*h_x[k])/(T_k[k]-T_k[k-1]))
+  }
+}
+
+### Edward's DRAFT functions END ###
+
 
 get_z_k <- function(T_k) { #Zixiao
   z_k <- vector()
